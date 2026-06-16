5e755c67aafd4081bd4f7aeabe3386961b6c2c07
Mihret Tadesse <mercytadesse40@gmail.com>
2026-06-16 23:09:37 +0300

    created navbar

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+import Navbar from "@/components/Navbar"
+
+export default function Home(){
+
+return (
+
+<main>
+
+<Navbar />
+
+<h1>
+Welcome to our Beauty Salon
+</h1>
+
+
+</main>
+
+)
+
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+export default function Navbar(){
+
+return (
+<nav className="flex justify-between p-6">
+
+<h1 className="text-2xl">
+Eden Beauty 
+</h1>
+
+<div className="space-x-5">
+
+<a>Home</a>
+<a>Services</a>
+<a>Gallery</a>
+<a>Contact</a>
+
+</div>
+
+</nav>
+)
+
+}
